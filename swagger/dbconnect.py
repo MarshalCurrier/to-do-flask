d170b963ea0ff694bd7dd4557744463631399ffb
Marshal Currier <marshalcurrier@gmail.com>
2024-05-01 19:16:20 -0700

    Updated connection string

--- swagger/dbconnect.py
+++ swagger/dbconnect.py
@@ -1,3 +1,3 @@
 from sqlalchemy import create_engine
 
-engine = create_engine("postgresql+psycopg2://postgres-to-do:strong-password@localhost:5432/db")
+engine = create_engine("postgresql+psycopg2://productivitypals:Pr0duc71v17yp4l$@hirehumble.com:5432/productivitypals")
